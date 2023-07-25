56eebca9165719cea1e6a84a9ace23a49e83adda
camilareyes03 <camilareyes562@gmail.com>
2023-07-24 23:41:58 -0400

    sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,31 +78,12 @@
                             <a href="{{ route('limpieza') }}">Limpieza</a>
                         </li>
                     </ul>
-                </li>
+
                 <li>
                     <a href="asistencia">Asistencias</a>
                     <a href="horario">Horarios</a>
+                </li>
 
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false"
-                        class="dropdown-toggle">Pages</a>
-                    <ul class="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                            <a href="#">Page 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 3</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Portfolio</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
             </ul>
 
             <ul class="list-unstyled CTAs">
