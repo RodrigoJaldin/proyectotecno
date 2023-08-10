5ee647d4fb84fa56f7e7e4abc732e8f88417e09f
TillWurner <85416653+TillWurner@users.noreply.github.com>
2023-08-09 22:53:14 -0400

    foto fin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,12 @@
         </div>
     </div>
 
+    
+
 </x-app-layout>
+<div>
+    <img style="position: absolute;left: 40%; top: 30%; height: 60%; width: 40%;" src="https://scontent.flpb3-1.fna.fbcdn.net/v/t39.30808-6/273825073_360684779214440_8375043239309205996_n.jpg?_nc_cat=106&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=S1VJ73J_FScAX-F1HRc&_nc_ht=scontent.flpb3-1.fna&oh=00_AfAyPzmxizAn80vJuxpgP7_XGZBLw2vPDPsx8vnBNmkcqg&oe=64DA22C8">
+</div>
 <div class="row">
     <div class="col">
         <div style="position: fixed; bottom: 10px; right: 10px;">
